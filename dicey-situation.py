46d6e5e0be6c6969405b41814b60b7cdb492fbf3
Zecong Hu <huzecong@gmail.com>
2020-08-06 23:35:34 -0400

    Update dicey-situation

--- dicey-situation.py
+++ dicey-situation.py
@@ -2,6 +2,8 @@ import itertools
 import math
 from typing import Dict, Iterable, List, Tuple
 
+EPS = 1e-7
+
 
 def product(xs: Iterable[float]) -> float:
     ret = 1.
@@ -16,112 +18,91 @@ def cmp(a, b):
     return 0
 
 
+# @profile
 def solution(dice_vals: List[int], n_dice: int, n_rolls: int):
     dice_vals = sorted(dice_vals)
     n_face = len(dice_vals)
 
-    # Alice: Targets maximum sum. Each dice is independent. Reroll whenever outcome is less than the expected value.
-    p_alice_dice: List[List[float]] = [[]]  # p_alice_dice[r][f]: prob. of ending up with face f after r rolls
-    p_alice_dice.append([1 / n_face] * n_face)
-    for r in range(2, n_rolls + 1):
-        probs = [0.] * n_face
-        p_reroll = 0
-        expected = sum(prob * val for prob, val in zip(p_alice_dice[-1], dice_vals))
-        for face in range(n_face):
-            if dice_vals[face] <= expected:
-                p_reroll += p_alice_dice[-1][face]
-            else:
-                probs[face] += p_alice_dice[-1][face]
-        for face in range(n_face):
-            probs[face] += p_reroll / n_face
-        p_alice_dice.append(probs)
-
-    max_sum = dice_vals[-1] * n_dice
-    p_alice_sum = [1.] + [0.] * max_sum
-    for _ in range(n_dice):
-        next_p_alice_sum = [0.] * (max_sum + 1)
-        for s in range(max_sum + 1):
-            for prob, val in zip(p_alice_dice[n_rolls], dice_vals):
-                if s - val >= 0:
-                    next_p_alice_sum[s] += p_alice_sum[s - val] * prob
-        p_alice_sum = next_p_alice_sum
-    assert math.isclose(sum(p_alice_sum), 1.)
-
-    # Bob: Always reroll dices with lowest face value. Targets maximum expected payout.
     # State is represented as a tuple of #{face values}, indicating #dices with each face
     # > p_roll_state[d][state]: prob. of getting `state` after rolling d dice
-    p_roll_state: List[Dict[Tuple[int, ...], float]] = []
+    p_roll_state: List[Dict[int, float]] = []
+    max_state = n_dice << (3 * (n_face - 1))
+    state_sum: Dict[int, int] = {}
+    state_transitions: List[List[int]] = [0] * (max_state + 1)
     for n_dice_to_roll in range(n_dice + 1):
         p_cur_state = {}
         p_base_coef = (1 / n_face) ** n_dice_to_roll * math.factorial(n_dice_to_roll)
         for seps in itertools.combinations(range(n_dice_to_roll + n_face - 1), n_face - 1):
             seps = [-1] + list(seps) + [n_dice_to_roll + n_face - 1]
-            state = tuple(r - l - 1 for l, r in zip(seps, seps[1:]))
+            state = [r - l - 1 for l, r in zip(seps, seps[1:])]
+            state_id = sum(x << (3 * i) for i, x in enumerate(state))
             prob = p_base_coef / product(math.factorial(x) for x in state)
-            p_cur_state[state] = prob
+            p_cur_state[state_id] = prob
+
+            if n_dice_to_roll == n_dice:
+                sum_val = sum(cnt * val for cnt, val in zip(state, dice_vals))
+                state_sum[state_id] = sum_val
+                transitions = []
+                next_state_id = state_id
+                idx = 0
+                for n_reroll in range(n_dice + 1):
+                    if n_reroll > 0:
+                        while state[idx] == 0: idx += 1
+                        state[idx] -= 1
+                        next_state_id -= 1 << (3 * idx)
+                    transitions.append(next_state_id)
+                state_transitions[state_id] = transitions
+
         assert math.isclose(sum(p_cur_state.values()), 1.)
         p_roll_state.append(p_cur_state)
-    state_sum: Dict[Tuple[int, ...], int] = {}
-    for state in p_roll_state[n_dice].keys():
-        sum_val = sum(cnt * val for cnt, val in zip(state, dice_vals))
-        state_sum[state] = sum_val
-    ans = 0.
-    for alice_val, alice_prob in enumerate(p_alice_sum):
-        if alice_prob == 0.: continue
+
+    max_sum = dice_vals[-1] * n_dice
+    valid_sums = [True] + [False] * max_sum
+    for _ in range(n_dice):
+        next_valid_sum = [False] * (max_sum + 1)
+        for s in range(max_sum + 1):
+            if not valid_sums[s]: continue
+            for val in dice_vals:
+                if s + val <= max_sum:
+                    next_valid_sum[s + val] = True
+        valid_sums = next_valid_sum
+
+    # Bob: Always reroll dices with lowest face value. Targets maximum expected payout.
+    expected_payout: Dict[int, float] = {}  # expected payout for Bob given Alice's sum
+    for alice_val in range(max_sum + 1):
+        if not valid_sums[alice_val]: continue
         # > exp_bob[(#face0, #face1, ...)]: expected outcome for Bob given state
         exp_bob = prev_exp_bob = {state: cmp(sum_val, alice_val) for state, sum_val in state_sum.items()}
         for r in range(2, n_rolls + 1):
             exp_bob = {}
             for state in p_roll_state[n_dice].keys():
                 choices = []
-                next_state = list(state)
-                idx = 0
-                for n_reroll in range(n_dice + 1):
-                    if n_reroll > 0:
-                        while next_state[idx] == 0: idx += 1
-                        next_state[idx] -= 1
+                for n_reroll, next_state in enumerate(state_transitions[state]):
                     expected = 0.
                     for roll_state, roll_prob in p_roll_state[n_reroll].items():
-                        potential_state = tuple(a + b for a, b in zip(next_state, roll_state))
+                        potential_state = next_state + roll_state
                         expected += prev_exp_bob[potential_state] * roll_prob
                     choices.append(expected)
-                print(alice_val, alice_prob, r, state, choices)
                 exp_bob[state] = max(choices)
             prev_exp_bob = exp_bob
         cur_exp = sum(prob * exp_bob[state] for state, prob in p_roll_state[n_dice].items())
-        ans += alice_prob * cur_exp
+        expected_payout[alice_val] = cur_exp
 
-    """
-    for alice_val, alice_prob in enumerate(p_alice_sum):
-        if alice_prob == 0.: continue
-        # > exp_bob[(#face0, #face1, ...)]: prob. of state in current roll
-        exp_bob = p_roll_state[n_dice]
-        for r in range(2, n_rolls + 1):
-            next_p_bob = defaultdict(float)
-            for state, prob in exp_bob:
-                sum_val = state_sum[state]
-                idx = 0
-                state = list(state)
-                for n_reroll in range(n_dice):
-                    if sum_val > alice_val: break  # reroll just enough dice to be greater than Alice's sum
-                    while state[idx] == 0: idx += 1
-                    new_sum_val = sum_val - dice_vals[idx] + expected_face[n_rolls - r + 1]
-                    # > if rerolling this dice leads to worse results, then prefer a tie instead
-                    if new_sum_val < sum_val == alice_val: break
-                    state[idx] -= 1
-                else:
-                    n_reroll = n_dice
-                for roll_state, roll_prob in p_roll_state[n_reroll]:
-                    next_state = tuple(a + b for a, b in zip(state, roll_state))
-                    next_p_bob[next_state] += prob * roll_prob
-            exp_bob = list(next_p_bob.items())
-        cur = 0.
-        print(alice_val, alice_prob, exp_bob)
-        for state, bob_prob in exp_bob:
-            bob_val = state_sum[state]
-            cur += (1 if bob_val > alice_val else -int(bob_val < alice_val)) * bob_prob
-        ans += alice_prob * cur
-    """
+    # Alice
+    exp_alice = prev_exp_alice = {state: expected_payout[sum_val] for state, sum_val in state_sum.items()}
+    for r in range(2, n_rolls + 1):
+        exp_alice = {}
+        for state in p_roll_state[n_dice].keys():
+            choices = []
+            for n_reroll, next_state in enumerate(state_transitions[state]):
+                expected = 0.
+                for roll_state, roll_prob in p_roll_state[n_reroll].items():
+                    potential_state = next_state + roll_state
+                    expected += prev_exp_alice[potential_state] * roll_prob
+                choices.append(expected)
+            exp_alice[state] = min(choices)
+        prev_exp_alice = exp_alice
+    ans = sum(prob * exp_alice[state] for state, prob in p_roll_state[n_dice].items())
     return f"{ans:.7f}"[2:]
 
 
@@ -139,13 +120,13 @@ def main():
     with open("data/sl_dicey_situation.pkl", "rb") as f:
         dice_vals, d, r = pickle.load(f)
 
-    # dice_vals = [random.randint(1, 20) for _ in range(6)]
-    # d = 8
-    # r = 8
-
-    dice_vals = [1, 2, 3]
+    dice_vals = [random.randint(1, 20) for _ in range(6)]
     d = 8
     r = 8
+
+    # dice_vals = [2, 3, 4, 5]
+    # d = 5
+    # r = 5
 
     print(dice_vals, d, r)
 
